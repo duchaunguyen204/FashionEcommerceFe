ef575f9f18aa742b57e9d4fd9b3f2b3d9283446a
duchaunguyen204 <haundph34804@fpt.edu.vn>
2024-10-02 17:55:27 +0700

    sửa lại lỗi buuton

--- DATN/src/ProductsLimited/ProductsLimited.jsx
+++ DATN/src/ProductsLimited/ProductsLimited.jsx
@@ -33,47 +33,53 @@ const ProductsLimited = () => {
         }
     ];
 
-  
-        return (
-            <div className="flex justify-center items-center h-screen">
-                <div className="flex space-x-4">
-                    <button className="text-gray-500">
-                        <i className="fas fa-chevron-left"></i>
-                    </button>
-                    {products.map((product, index) => (
-                        <div key={index} className="text-left">
+
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex space-x-4">
+                <button className="text-gray-500">
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+                {products.map((product, index) => (
+                    <div key={index} className="text-left">
+                        <div className="relative">
                             <img src={product.image} alt={product.alt} className="w-64 h-64 object-cover mb-4" />
-                            <p className="text-gray-500">{product.category}</p>
-                            <p className="font-semibold">{product.name}</p>
-                            <p className="text-gray-700">{product.price}</p>
-                            {product.reviews && (
-                                <div className="flex items-center text-yellow-500">
-                                    <i className="fas fa-star"></i>
-                                    <i className="fas fa-star"></i>
-                                    <i className="fas fa-star"></i>
-                                    <i className="fas fa-star"></i>
-                                    <i className="fas fa-star"></i>
-                                    <span className="text-gray-500 ml-2">{product.reviews}</span>
-                                </div>
-                            )}
-                            <button className="text-gray-500 mt-2">
-                                <i className="far fa-heart"></i>
+                            <button className="absolute bottom-4 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2">
+                                ADD TO CART
                             </button>
                         </div>
-                    ))}
-                    <button className="text-gray-500">
-                        <i className="fas fa-chevron-right"></i>
-                    </button>
-                </div>
-                <div className="absolute bottom-16 flex space-x-2">
-                    <span className="w-3 h-3 bg-pink-200 rounded-full"></span>
-                    <span className="w-3 h-3 bg-pink-200 rounded-full"></span>
-                    <span className="w-3 h-3 bg-gray-900 rounded-full"></span>
-                </div>
+
+                        <p className="text-gray-500">{product.category}</p>
+                        <p className="font-semibold">{product.name}</p>
+                        <p className="text-gray-700">{product.price}</p>
+                        {product.reviews && (
+                            <div className="flex items-center text-yellow-500">
+                                <i className="fas fa-star"></i>
+                                <i className="fas fa-star"></i>
+                                <i className="fas fa-star"></i>
+                                <i className="fas fa-star"></i>
+                                <i className="fas fa-star"></i>
+                                <span className="text-gray-500 ml-2">{product.reviews}</span>
+                            </div>
+                        )}
+                        <button className="text-gray-500 mt-2">
+                            <i className="far fa-heart"></i>
+                        </button>
+                    </div>
+                ))}
+                <button className="text-gray-500">
+                    <i className="fas fa-chevron-right"></i>
+                </button>
             </div>
-        );
-    }
-    
+            <div className="absolute bottom-16 flex space-x-2">
+                <span className="w-3 h-3 bg-pink-200 rounded-full"></span>
+                <span className="w-3 h-3 bg-pink-200 rounded-full"></span>
+                <span className="w-3 h-3 bg-gray-900 rounded-full"></span>
+            </div>
+        </div>
+    );
+}
+
 
 
 export default ProductsLimited
